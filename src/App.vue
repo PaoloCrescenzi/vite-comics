e1b447b5698bca0d49b2962816358c5a1a94dd29
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-12-05 17:43:37 +0100

    inizio progetto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import ComHeader from './components/ComHeader.vue'
+</script>
+
+<template>
+  <ComHeader/>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav >
+    <div class="container">
+          <img src="../../img/dc-logo.png" alt="">
+
+          
+    </div>
+  </nav>
+</template>
+
+
+
+<script setup>
+
+
+</script>
+
+
+
+
+<style scoped>
+
+</style>
